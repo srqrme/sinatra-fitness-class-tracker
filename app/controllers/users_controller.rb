778a9092614919b99087ec299f1176e0d3f313af
Sharmaine Requerme <srqrme@gmail.com>
2019-02-12 17:58:03 +0000

    fix logged_in? and current_user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if logged_in?
-      redirect to '/fitness_classes'
+      redirect to '/fitness_classes/'
     else
       erb :'users/signup'
     end
