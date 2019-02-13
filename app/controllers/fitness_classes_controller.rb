ce7cd80fd617aa85a3156a83afff5fc22ba45d34
Sharmaine Requerme <srqrme@gmail.com>
2019-02-13 19:57:32 +0000

    add get '/fitness_classes/:id/edit' route

--- app/controllers/fitness_classes_controller.rb
+++ app/controllers/fitness_classes_controller.rb
@@ -37,7 +37,20 @@ class FitnessClassesController < ApplicationController
   get '/fitness_classes/:id' do
     if logged_in?
       @fitness_class = FitnessClass.find_by_id(params[:id])
-      erb :'fitness_classes/show.erb'
+      erb :'fitness_classes/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/fitness_classes/:id/edit' do
+    if logged_in?
+      @fitness_class = FitnessClass.find_by_id(params[:id])
+      if @fitness_class.user_id == current_user.id
+        erb :'/fitness_classes/edit'
+      else
+        redirect to "/fitness_classes"
+      end
     else
       redirect to '/login'
     end
