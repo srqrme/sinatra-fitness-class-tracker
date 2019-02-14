24b25b9e95678705a6fe2c9f99763bcc3d39c3e9
Sharmaine Requerme <srqrme@gmail.com>
2019-02-14 19:07:46 +0000

    fixed files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :fitness_classes
   has_secure_password
+
+  PASSWORD_FORMAT = /\A
+  (?=.{6,})          # Must contain 6 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+/x
+
+  validates :password, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }, allow_nil: true
 end
